01683a1470c522403fcd0412ab4baf85f7fe1bac
Byron Hernandez <145072285+rokoisloco@users.noreply.github.com>
2024-10-10 15:22:02 -0700

    finished 1.3
    ok

--- VrRoomHernandezP7/Assets/_Course Library/ReticleController.cs
+++ VrRoomHernandezP7/Assets/_Course Library/ReticleController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class ReticleController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private GameObject reticleSquare;
+    private float scaleSpeed = 0.1f;
+    private float maxScale = 1.2f;
+    private float minScale = 1f;
+    private Vector3 vector3;
+    private readonly float speed = 50.0f;
 
-    // Update is called once per frame
-    void Update()
+    private bool isScalingUp = true;
+
+    private void Update()
     {
-        
+        // Rotate the reticle around the y-axis
+        if (Application.isPlaying)
+        {
+            vector3 = new Vector3(0, 1, 0);
+            transform.Rotate(speed * Time.deltaTime * vector3);
+        }
+
+        // Scale the reticle up and down
+        if (isScalingUp)
+        {
+            transform.localScale += Vector3.one * scaleSpeed * Time.deltaTime;
+            if (transform.localScale.x >= maxScale)
+            {
+                isScalingUp = false;
+            }
+        }
+        else
+        {
+            transform.localScale -= Vector3.one * scaleSpeed * Time.deltaTime;
+            if (transform.localScale.x <= minScale)
+            {
+                isScalingUp = true;
+            }
+        }
     }
 }
